9292537ba69b60b6276fc6a300eddb088174c51c
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2026-03-09 17:22:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// let day = 1;
+
+// switch(day){
+//     case 0:
+//         console.log("monday");
+//     case 1:
+//         console.log("tuesday");
+//           break;
+//     case 2:
+//         console.log("wednesday");
+//         break;
+//     case 3:
+//         console.log("thursday");
+//     case 4:
+//         console.log("friday");
+//     case 5:
+//         console.log("saturday");
+    
+//     default:
+//         console.log("Ayan yusuf zai is back");
+
+// //     }
+
+// let i = 0;
+
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+// console.log(`the current value of i is ${i}`);
+
+// const numbers = [1,2,3,4,5]
+
+// const double = numbers.map( num => num*2);
+// const double = numbers.map( function(num){
+//     return num *2;     higher order function which can accept a function as an argument or it can be return a function as well is knownn as high order function 
+// })
+
+// console.log(double)
+
+
+// document.getElementById("topDiv").addEventListener("click", ()=> {
+//     console.log("outer div has been clicked");
+// },true)
+
+
+// document.getElementById("midDiv").addEventListener("click", ()=> {
+//     console.log("mid div has been clicked");
+// },true)
+
+// document.getElementById("innerDiv").addEventListener("click", ()=> {
+//     console.log("Inner div has been clicked");
+// },true)
+
+// IIFE 
+
+// function greet(){
+    // console.log("Ayan");
+// }
+// greet(name);
+// (function (name) {
+//     console.log("assalam alaikum " + name );
+// })("Ayan");
